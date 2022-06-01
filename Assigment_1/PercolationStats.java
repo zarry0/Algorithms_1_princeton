f58d7bf48f22076f3b218443c3a588f99806c9cf
Rodrigo Weller Zarrabal <cs.rgzarrabal@gmail.com>
2022-05-31 19:05:22 -0500

    Unused libraries removed

--- Assigment_1/PercolationStats.java
+++ Assigment_1/PercolationStats.java
@@ -1,6 +1,3 @@
-import java.time.DayOfWeek;
-
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
