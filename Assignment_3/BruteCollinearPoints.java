db0a45dc851af8c36b6d6558889a232e82bf3055
Rodrigo Weller Zarrabal <cs.rgzarrabal@gmail.com>
2022-07-06 19:54:25 -0500

    Minor code arrangement change

--- Assignment_3/BruteCollinearPoints.java
+++ Assignment_3/BruteCollinearPoints.java
@@ -1,9 +1,7 @@
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
-
-import java.util.Arrays;
-
 import edu.princeton.cs.algs4.In;
+import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
